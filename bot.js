b3b53b103cc4e4a8e620d4d924a43fbd2942cc85
Mateusz Biały <mateusz.bialy@pm.me>
2019-09-14 09:59:43 +0200

    Shortened if-then statements

--- bot.js
+++ bot.js
@@ -66,42 +66,36 @@ function getRandomDelay() {
 
 //Auto Answer
 window.setInterval(() => {
-    if($('input[name="autoAnswer"]:checked').length > 0) {
+    if($('input[name="autoAnswer"]:checked').length > 0)
         $("#answer").val(getAnswer());
-    }
 }, autoAnswerDelay);
 
 //Auto Check
 window.setInterval(() => {
-    if($('input[name="autoCheck"]:checked').length > 0 && $("#answer_result").is(":visible") == false && $("#summary").is(":visible") == false && $("#answer").val() != "") {
+    if($('input[name="autoCheck"]:checked').length > 0 && !$("#answer_result").is(":visible") && !$("#summary").is(":visible") && $("#answer").val())
         $("#check").click();
-    }
 }, getRandomDelay());
 
 //Auto Next Word
 window.setInterval(() => {
-    if($('input[name="autoNextWord"]:checked').length > 0 && $("#answer_result").is(":visible") == true && $("#summary").is(":visible") == false) {
+    if($('input[name="autoNextWord"]:checked').length > 0 && $("#answer_result").is(":visible") && !$("#summary").is(":visible"))
         $("#nextword").click();
-    }
 }, autoNextWordDelay);
 
 //Auto New Word
 window.setInterval(() => {
-    if($('input[name="autoNewWord"]:checked').length > 0 && $("#new_word_form").is(":visible") == true) {
+    if($('input[name="autoNewWord"]:checked').length > 0 && $("#new_word_form").is(":visible"))
         $("#dont_know_new").click();
-    }
 }, autoNewWordDelay);
 
 //Auto Skip
 window.setInterval(() => {
-    if($('input[name="autoSkip"]:checked').length > 0 && $(".possible_word_msg").is(":visible") == true) {
+    if($('input[name="autoSkip"]:checked').length > 0 && $(".possible_word_msg").is(":visible"))
         $("#skip").click();
-    }
 }, autoSkipDelay);
 
 //Auto Finish
 window.setInterval(() => {
-    if($('input[name="autoFinish"]:checked').length > 0 && $("#summary").is(":visible") == true) {
+    if($('input[name="autoFinish"]:checked').length > 0 && $("#summary").is(":visible"))
         $("#return_mainpage").click();
-    }
 }, autoFinishDelay)
